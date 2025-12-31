fbf4dd4dd458e43cf075fb69311e1ea0945d87dc
crisstinagl <crisgdl3011@gmail.com>
2025-12-31 15:03:41 +0100

    Ajustes para dislexicos implementados

--- Assets/Scripts/ScenesNav/ScenesManager.cs
+++ Assets/Scripts/ScenesNav/ScenesManager.cs
@@ -1,4 +1,6 @@
 using System.Collections;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Localization.Settings;
 using UnityEngine.SceneManagement;
@@ -11,8 +13,22 @@ public class ScenesManager : MonoBehaviour
     [Header("Ajustes de Audio")]
     public AudioSource musicaSource;
 
+    [System.Serializable]
+    public struct AccesibilityFont
+    {
+        public TMP_FontAsset originalFont;
+        public TMP_FontAsset dyslexicFont;
+    };
+
+    [Header("Mapeo de Fuentes")]
+    public List<AccesibilityFont> fontCatalog;
+    private Dictionary<TMP_Text, TMP_FontAsset> fontsMemory = new Dictionary<TMP_Text, TMP_FontAsset>();
+
+    private bool activeDyslexicMode = false;
+
     [HideInInspector] public float actualVol;
     [HideInInspector] public float actualBrightness;
+    [HideInInspector] public bool isDyslexicMode;
 
     void Awake()
     {
@@ -30,6 +46,43 @@ public class ScenesManager : MonoBehaviour
         else { Destroy(gameObject); }
     }
 
+    // FUNCIONES PARA LA GESTION DE LA FUENTE PARA DISLEXICOS
+    public void ApplyDislexicFont(bool activate)
+    {
+        activeDyslexicMode = activate;
+        PlayerPrefs.SetInt("AccesibilidadDislexia", activate ? 1 : 0);
+
+        TMP_Text[] textos = Resources.FindObjectsOfTypeAll<TMP_Text>();
+
+        foreach (var txt in textos)
+        {
+            UpdateText(txt);
+        }
+    }
+
+    public void UpdateText(TMP_Text text)
+    {
+        foreach (var par in fontCatalog)
+        {
+            if (activeDyslexicMode)
+            {
+                if (text.font == par.originalFont)
+                {
+                    text.font = par.dyslexicFont;
+                    break;
+                }
+            }
+            else
+            {
+                if (text.font == par.dyslexicFont)
+                {
+                    text.font = par.originalFont;
+                    break;
+                }
+            }
+        }
+    }
+
     // Funcion para reproducir la musica
     private void PlayMusic()
     {
@@ -41,22 +94,25 @@ public class ScenesManager : MonoBehaviour
         }
     }
 
-
-    // FUNCIONES PARA CARGAR Y APLICAR LOS AJUSTES DE BRILLO Y VOLUMEN
+    // FUNCIONES PARA CARGAR Y APLICAR LOS AJUSTES
     private void LoadSettings()
     {
         actualVol = PlayerPrefs.GetFloat("Volumen", 0.5f);
         actualBrightness = PlayerPrefs.GetFloat("Brillo", 1.0f);
+        isDyslexicMode = PlayerPrefs.GetInt("Dislexia", 0) == 1;
+
         ApplyChanges();
     }
 
-    public void UpdateValues(float vol, float bright)
+    public void UpdateValues(float vol, float bright, bool dyslexic)
     {
         actualVol = vol;
         actualBrightness = bright;
+        isDyslexicMode = dyslexic;
 
         PlayerPrefs.SetFloat("Volumen", vol);
         PlayerPrefs.SetFloat("Brillo", bright);
+        PlayerPrefs.SetInt("Dislexia", dyslexic ? 1 : 0);
         PlayerPrefs.Save();
 
         ApplyChanges();
@@ -64,8 +120,10 @@ public class ScenesManager : MonoBehaviour
 
     public void ApplyChanges()
     {
+        // Volumen
         AudioListener.volume = actualVol;
 
+        // Brillo
         GameObject overlay = GameObject.FindWithTag("BrilloOverlay");
         if (overlay != null)
         {
@@ -74,11 +132,45 @@ public class ScenesManager : MonoBehaviour
             c.a = Mathf.Clamp(1.0f - actualBrightness, 0f, 0.8f);
             img.color = c;
         }
+
+        // Fuente dislexia
+        TMP_Text[] allTexts = Resources.FindObjectsOfTypeAll<TMP_Text>();
+        foreach (var text in allTexts)
+        {
+            if (text == null || text.gameObject.hideFlags == HideFlags.NotEditable || text.gameObject.hideFlags == HideFlags.HideAndDontSave)
+                continue;
+
+            if (!fontsMemory.ContainsKey(text))
+            {
+                fontsMemory.Add(text, text.font);
+            }
+
+            TMP_FontAsset originalTextFont = fontsMemory[text];
+
+            foreach (var mapping in fontCatalog)
+            {
+                if (isDyslexicMode)
+                {
+                    if (originalTextFont == mapping.originalFont)
+                    {
+                        text.font = mapping.dyslexicFont;
+                        break;
+                    }
+                }
+                else
+                {
+                    text.font = originalTextFont;
+                    break;
+                }
+            }
+            text.SetAllDirty();
+        }
     }
 
     // Funcion para cambiar de escena
     public void ChangeScene(string nombre)
     {
+        fontsMemory.Clear();
         Time.timeScale = 1; // Siempre resetear el tiempo al cambiar
         SceneManager.LoadScene(nombre);
     }
--- Assets/Scripts/Settings/Settings.cs
+++ Assets/Scripts/Settings/Settings.cs
@@ -2,25 +2,32 @@ using UnityEngine;
 using UnityEngine.UI;
 
 // Clase para gestionar los ajustes
-public class Seetings : MonoBehaviour
+public class Settings : MonoBehaviour
 {
     [Header("Referencias")]
     public Slider volSlider;
     public Slider brightnessSlider;
+    public Toggle dyslexiaToggle;
 
     void Start()
     {
         volSlider.value = ScenesManager.Instance.actualVol;
         brightnessSlider.value = ScenesManager.Instance.actualBrightness;
+        dyslexiaToggle.isOn = ScenesManager.Instance.isDyslexicMode;
 
         volSlider.onValueChanged.AddListener(delegate { NotifyChange(); });
         brightnessSlider.onValueChanged.AddListener(delegate { NotifyChange(); });
+        dyslexiaToggle.onValueChanged.AddListener(delegate { NotifyChange(); });
 
         ScenesManager.Instance.ApplyChanges();
     }
 
     public void NotifyChange()
     {
-        ScenesManager.Instance.UpdateValues(volSlider.value, brightnessSlider.value);
+        ScenesManager.Instance.UpdateValues(
+            volSlider.value, 
+            brightnessSlider.value,
+            dyslexiaToggle.isOn
+        );
     }
 }
